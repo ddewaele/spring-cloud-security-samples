69618ec2808b2c3cecb231b3a0e61ba24e52d34e
Davy De Waele <ddewaele@gmail.com>
2016-03-24 12:44:17 +0100

    added html files / moved to snapshot

--- sample1/ui/src/main/java/demo/UiApplication.java
+++ sample1/ui/src/main/java/demo/UiApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.web.csrf.CsrfFilter;
 import org.springframework.security.web.csrf.CsrfToken;
 import org.springframework.security.web.csrf.CsrfTokenRepository;
@@ -20,6 +21,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @SpringBootApplication
+@EnableResourceServer
 public class UiApplication extends WebSecurityConfigurerAdapter {
 
 	public static void main(String[] args) {
